6f59426b05eab149a4046029ccac0e6be66f5467
CmlSph <sph_cml1974@outlook.com>
2021-05-31 10:44:27 +0200

    react_router_tutorial using PokemonAPI

---
+++ src/ItemDetail.js
@@ -0,0 +1,32 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+function Item({ match }) {
+  useEffect(() => {
+    fetchItem();
+    console.log(match);
+  }, []);
+
+  const [item, setItem] = useState({
+      sprites:{}
+  });
+    
+
+  const fetchItem = async () => {
+    const fetchItem = await fetch(
+      `https://pokeapi.co/api/v2/pokemon/${match.params.name}`
+    );
+    const item = await fetchItem.json();
+    setItem(item);
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <h1>{item.name}</h1>
+      <img src={item.sprites.front_default} alt="" />
+    </div>
+  );
+}
+
+export default Item;
